b7655c3e9c365e0c44483b934f647f6ffafd36df
Cuong Desktop <nguyenc72694@gmail.com>
2020-08-06 20:19:53 -0500

    added pictures and formated project section

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // Transition effect for navbar
+    $(window).scroll(function () {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      var height = $(".masthead").height();
+      var scrollTop = $(window).scrollTop();
+
+      if (scrollTop >= height - 40) {
+        $(".navbar").addClass("solid");
+      } else {
+        $(".navbar").removeClass("solid");
+      }
+    });
+  });
